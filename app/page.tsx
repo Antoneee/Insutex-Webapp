4a741df83c34a7f93cabec985889ee96da05cf70
Anthony Ngo <anthtngo@amazon.com>
2025-05-24 17:31:49 -0700

    Add Who We Are section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import WhoWeAreImage from "@/public/home/who-we-are.png";
+import Button from "./components/Button";
+
 export default function Home() {
   return (
     <main>
@@ -5,6 +9,18 @@ export default function Home() {
         <div className="bg-hero bg-cover bg-center p-20">
           <h1 className="text-neutral-white mt-20">Insutex Industries Sdn. BhD.</h1>
           <p className="text-3xl text-neutral-white">Moving Goods, Growing Partnerships</p>
+        </div>
+      </section>
+      <section>
+        <div className="flex items-center gap-20 p-20">
+          <div className="w-3/5">
+            <Image src={WhoWeAreImage} alt="Who We Are" className="w-full h-auto object-cover"/>
+          </div>
+          <div className="w-2/5 text-left">
+            <h2 className="uppercase mb-4">Who We Are</h2>
+            <p className="text-lg mb-8">As a trusted supplier to industrial sectors, we provide essential materials and equipment for foundry, smelter, machining, and oil & gas industries, supporting projects across Asia.</p>
+            <Button borderColor="border-primary" textColor="text-primary">About Us</Button>
+          </div>
         </div>
       </section>
     </main>
