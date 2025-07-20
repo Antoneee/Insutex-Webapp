135eed22391c17d90c3c5523623550d1327fdc07
Anthony Ngo <anthtngo@amazon.com>
2025-07-20 02:51:12 -0700

    Update config to limit resources used during building process

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+
+// Initialize dotenv
+dotenv.config();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: 'build',
+  experimental: {
+    // This is experimental but can
+    // be enabled to allow parallel threads
+    // with nextjs automatic static generation
+    workerThreads: false,
+    cpus: 4
+  }
 };
 
 export default nextConfig;
